14f93cd4f38b33426bbc56dfd35ed40e9e08a4e5
littlexiaocai <13675150436cc@163.com>
2017-08-12 02:41:00 +0800

    my project push

--- chap0/project/guess_num_pro.py
+++ chap0/project/guess_num_pro.py
@@ -1,7 +1,21 @@
 
 import random
 
-key = [random.randint(1,9),random.randint(0,9),random.randint(0,9),random.randint(0,9)]
+# 产生一个随机4位数
+def gener_key():
+    key =[0,0,0,0]
+    key[0] = random.randint(1,9)
+    key[1] = random.randint(0,9)
+    while key[0]==key[1]:
+        key[1] = random.randint(0,9)
+    key[2] = random.randint(0,9)
+    while (key[0]==key[2]) or (key[1]==key[2]):
+        key[2] = random.randint(0,9)
+    key[3] = random.randint(0,9)
+    while (key[0]==key[3]) or (key[1]==key[3]) or (key[2]==key[3]):
+        key[3] = random.randint(0,9)
+
+    return key
 
 # 将输入的数字分解成一个列表
 def splitdigit(num):
@@ -12,21 +26,6 @@ def splitdigit(num):
         num = num%j
         j = j/10 
     return digits
-
-#def comparedigit(key,digits):
-#    a = 0
-#    b = 0
-#    for i in range(0,4):
-#        for j in range(0,4):
-#            if key[i] == digits[j]:
-#                if i == j:
-#                    a += 1
-#                    break
-#                else:
-#                    b += 1
-#                    break
-#    print("%dA%dB"%(a,b))     
-#    return (a,b)
 
 #将输入数字和答案进行逐一比较
 def comparedigit(key,digits):
@@ -46,6 +45,7 @@ def comparedigit(key,digits):
 
 i = 10
 
+key = gener_key() 
 print("答案是",key)
 
 while True:
