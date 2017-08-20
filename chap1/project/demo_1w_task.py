a0d1a5a6b1c85173ffc1bd58d32e6e755651d12a
littlexiaocai <13675150436cc@163.com>
2017-08-20 12:18:37 +0800

    my project push

--- chap1/project/demo_1w_task.py
+++ chap1/project/demo_1w_task.py
@@ -4,6 +4,7 @@
 输入指令，退出程序的交互
 在退出程序之前，打印查询过的所有城市。
 '''
+import csv
 
 def weather_dict_change():
     with open("weather_info.txt") as f:
@@ -15,22 +16,37 @@ def weather_dict_change():
 
     return weather_dict
 
+
+def csv_weather_dict(filename):
+    with open(filename,newline = '') as f:
+        weather_dict = {}
+        weather_file = csv.reader(f)       #CSV读取后返回列表
+        for city,weather in weather_file:
+            weather_dict[city] = weather
+
+    return weather_dict
+
+
 def main():
-    weather_dict = weather_dict_change()
+    #weather_dict = weather_dict_change()
+    weather_dict = csv_weather_dict("weather_info.txt")
     record = {}
     cmd = input("请输入城市名或指令:")
-    while cmd != "quit":
+    while True:
         if cmd in weather_dict:
             print("%s天气状况的天气状况为:%s"%(cmd,weather_dict[cmd]))
             record[cmd] = weather_dict[cmd]
-        elif cmd == "history":
+        elif cmd in ["history","his"]:
             for city,weather in record.items():
                 print(city,weather)
-        elif cmd == "help":
+        elif cmd in ["help","h"]:
             print("\n输入城市名，查询该城市的天气；\n")
             print("输入help，获取帮助文档；\n")
             print("输入history，获取查询历史；\n")
             print("输入quit，退出天气查询系统。\n")
+        elif cmd in ["quit","q"]:
+            print("程序退出")
+            break
         else:
             print("输入命令无效")
     
